bee062b5e2a7118385394822d223ff1b0fa74526
DiffuseHyperion <95083897+DiffuseHyperion@users.noreply.github.com>
2025-04-25 00:45:42 +0800

    feat: add ad components and an ad in challenge page

--- src/app/dashboard/challenges/[repository]/[challenge]/page.tsx
+++ src/app/dashboard/challenges/[repository]/[challenge]/page.tsx
@@ -3,6 +3,9 @@ import ChallengeView from "@/src/app/dashboard/challenges/[repository]/[challeng
 import {Skeleton} from "@/src/components/ui/skeleton"
 import {isUUID} from "@/src/lib/utils"
 import {notFound} from "next/navigation"
+import AdUnit from "@/src/components/ads/AdUnit"
+import {Card, CardHeader} from "@/src/components/ui/card"
+import AdPusher from "@/src/components/ads/AdPusher"
 
 export default async function ChallengesPage({params}: { params: Promise<{ repository: string, challenge: string }> }) {
     const repositoryId = (await params).repository
@@ -15,10 +18,22 @@ export default async function ChallengesPage({params}: { params: Promise<{ repos
     }
 
     return (
-        <div className={"grow padding small-column"}>
+        <div className={"grow padding flex gap-4 flex-col lg:flex-row"}>
+            <AdPusher/>
             <Suspense fallback={<Skeleton className={"grow"}/>}>
                 <ChallengeView repositoryId={repositoryId} challengeId={challengeId}/>
             </Suspense>
+            <Card className={"lg:w-1/4"}>
+                <CardHeader>
+                    <AdUnit client={"ca-pub-1971189389097192"}
+                            slot={"5679687143"}
+                            format={"autorelaxed"}
+                            data-matched-content-rows-num="3,1"
+                            data-matched-content-columns-num="1,2"
+                            data-matched-content-ui-type="image_stacked,image_stacked"
+                    />
+                </CardHeader>
+            </Card>
         </div>
     )
 }
