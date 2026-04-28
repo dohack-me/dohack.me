331683cc8a43aab497344e33cde5db7d02f0e05f
diffusehyperion <95083897+DiffuseHyperion@users.noreply.github.com>
2026-04-28 15:44:01 +0800

    recreate migrations since it was fucked last time, updated all deps what could go wrong

--- prisma/migrations/20260428071345_init/migration.sql
+++ prisma/migrations/20260428071345_init/migration.sql
@@ -41,6 +41,8 @@ CREATE TABLE "service" (
     "type" "ServiceType" NOT NULL,
     "image" TEXT NOT NULL,
     "tag" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "service_pkey" PRIMARY KEY ("id")
 );
@@ -60,6 +62,8 @@ CREATE TABLE "service_instance" (
 CREATE TABLE "solve" (
     "userId" UUID NOT NULL,
     "challengeId" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "solve_pkey" PRIMARY KEY ("userId","challengeId")
 );
@@ -70,6 +74,8 @@ CREATE TABLE "hint" (
     "challengeId" UUID NOT NULL,
     "title" TEXT NOT NULL,
     "hint" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "hint_pkey" PRIMARY KEY ("id")
 );
@@ -78,68 +84,67 @@ CREATE TABLE "hint" (
 CREATE TABLE "bookmark" (
     "userId" UUID NOT NULL,
     "challengeId" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "bookmark_pkey" PRIMARY KEY ("userId","challengeId")
 );
 
 -- CreateTable
-CREATE TABLE "user"
-(
+CREATE TABLE "user" (
     "id" UUID NOT NULL,
-    "name"          TEXT    NOT NULL,
+    "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "emailVerified" BOOLEAN NOT NULL DEFAULT false,
     "image" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "role"          TEXT    NOT NULL DEFAULT 'user',
+    "role" TEXT NOT NULL DEFAULT 'user',
 
     CONSTRAINT "user_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "session"
-(
-    "id"        UUID         NOT NULL,
+CREATE TABLE "session" (
+    "id" UUID NOT NULL,
     "expiresAt" TIMESTAMP(3) NOT NULL,
-    "token"     TEXT         NOT NULL,
+    "token" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "ipAddress" TEXT,
     "userAgent" TEXT,
-    "userId"    UUID         NOT NULL,
+    "userId" UUID NOT NULL,
 
     CONSTRAINT "session_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "account" (
-                           "id"                    UUID NOT NULL,
-                           "accountId"             TEXT NOT NULL,
-                           "providerId"            TEXT NOT NULL,
-                           "userId"                UUID NOT NULL,
-                           "accessToken"           TEXT,
-                           "refreshToken"          TEXT,
-                           "idToken"               TEXT,
-                           "accessTokenExpiresAt"  TIMESTAMP(3),
-                           "refreshTokenExpiresAt" TIMESTAMP(3),
+    "id" UUID NOT NULL,
+    "accountId" TEXT NOT NULL,
+    "providerId" TEXT NOT NULL,
+    "userId" UUID NOT NULL,
+    "accessToken" TEXT,
+    "refreshToken" TEXT,
+    "idToken" TEXT,
+    "accessTokenExpiresAt" TIMESTAMP(3),
+    "refreshTokenExpiresAt" TIMESTAMP(3),
     "scope" TEXT,
-                           "password"              TEXT,
+    "password" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-                           CONSTRAINT "account_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "account_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "verification"
-(
-    "id"         UUID         NOT NULL,
-    "identifier" TEXT         NOT NULL,
-    "value"      TEXT         NOT NULL,
-    "expiresAt"  TIMESTAMP(3) NOT NULL,
+CREATE TABLE "verification" (
+    "id" UUID NOT NULL,
+    "identifier" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt"  TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "verification_pkey" PRIMARY KEY ("id")
 );
@@ -163,19 +168,19 @@ CREATE UNIQUE INDEX "service_instance_userId_serviceId_key" ON "service_instance
 CREATE UNIQUE INDEX "hint_id_key" ON "hint"("id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "user_email_key" ON "user" ("email");
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
 
 -- CreateIndex
-CREATE INDEX "session_userId_idx" ON "session" ("userId");
+CREATE INDEX "session_userId_idx" ON "session"("userId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "session_token_key" ON "session" ("token");
+CREATE UNIQUE INDEX "session_token_key" ON "session"("token");
 
 -- CreateIndex
-CREATE INDEX "account_userId_idx" ON "account" ("userId");
+CREATE INDEX "account_userId_idx" ON "account"("userId");
 
 -- CreateIndex
-CREATE INDEX "verification_identifier_idx" ON "verification" ("identifier");
+CREATE INDEX "verification_identifier_idx" ON "verification"("identifier");
 
 -- AddForeignKey
 ALTER TABLE "challenge" ADD CONSTRAINT "challenge_repositoryId_fkey" FOREIGN KEY ("repositoryId") REFERENCES "repository"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -184,15 +189,13 @@ ALTER TABLE "challenge" ADD CONSTRAINT "challenge_repositoryId_fkey" FOREIGN KEY
 ALTER TABLE "service" ADD CONSTRAINT "service_challengeId_fkey" FOREIGN KEY ("challengeId") REFERENCES "challenge"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "service_instance"
-    ADD CONSTRAINT "service_instance_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "service_instance" ADD CONSTRAINT "service_instance_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "service_instance" ADD CONSTRAINT "service_instance_serviceId_fkey" FOREIGN KEY ("serviceId") REFERENCES "service"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "solve"
-    ADD CONSTRAINT "solve_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "solve" ADD CONSTRAINT "solve_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "solve" ADD CONSTRAINT "solve_challengeId_fkey" FOREIGN KEY ("challengeId") REFERENCES "challenge"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -201,15 +204,13 @@ ALTER TABLE "solve" ADD CONSTRAINT "solve_challengeId_fkey" FOREIGN KEY ("challe
 ALTER TABLE "hint" ADD CONSTRAINT "hint_challengeId_fkey" FOREIGN KEY ("challengeId") REFERENCES "challenge"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "bookmark"
-    ADD CONSTRAINT "bookmark_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "bookmark" ADD CONSTRAINT "bookmark_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "bookmark" ADD CONSTRAINT "bookmark_challengeId_fkey" FOREIGN KEY ("challengeId") REFERENCES "challenge"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "session"
-    ADD CONSTRAINT "session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "session" ADD CONSTRAINT "session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "account" ADD CONSTRAINT "account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
